00179821bf18bd92167782566f936e8eadf7b3d5
daeho kim <daeho.kim@softwareinlife.com>
2021-06-22 13:14:58 +0900

    first commit

---
+++ src/main/java/study/datajpa/Practice2DataJpaApplication.java
@@ -0,0 +1,13 @@
+package study.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice2DataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practice2DataJpaApplication.class, args);
+    }
+
+}
