a3114ac82e865483fb3f52f08551f3884a06744b
daeho kim <daeho.kim@softwareinlife.com>
2021-06-23 18:12:59 +0900

    @NamedQuery finish

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -9,6 +9,10 @@ import javax.persistence.*;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //Entity는 기본생성자 필수. JPA가 proxying할때 접근하기 때문에 private는 안되고 protected까진 지원된다.
 @ToString(of = {"id", "username", "age"})
+@NamedQuery(
+        name="Member.findByUsername",
+        query="select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id
@@ -34,6 +38,11 @@ public class Member {
         if(team != null){
             changeTeam(team);
         }
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
     }
 
     public void changeTeam(Team team){
--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,7 +1,17 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    //Query annotation을 생략해도 관례상 Entity.MethodName과 일치하는 namedQuery가 있는지 확인하여 있으면 선적용한다. 없으면 메소드네이밍으로 실행한다.
+//    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
 }
